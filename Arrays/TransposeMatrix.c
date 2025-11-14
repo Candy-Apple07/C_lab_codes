a13d6cb3bf962a39d24a9f002e68b0d072af7a36
Shreyas <shreyas.rongde@gmail.com>
2025-11-14 09:28:38 +0530

    Create TransposeMatrix.c

---
+++ Arrays/TransposeMatrix.c
@@ -0,0 +1,24 @@
+// Shreyas Rongde (25070521084)
+
+#include <stdio.h>
+int main() {
+  int n;
+  scanf("%d", &n);
+  int matrix[n][n];
+  int i, j;
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+
+      scanf("%d", &matrix[i][j]);
+    }
+  }
+
+  for (i = 0; i < n; i++) {
+    for (j = 0; j < n; j++) {
+      printf("%d ", matrix[j][i]);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
